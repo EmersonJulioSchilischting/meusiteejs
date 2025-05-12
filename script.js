157d20ecc7fc0f6ca47c82311c0a3133f0eae66d
EmersonJulioSchilischting <emersonjulioschilischting@gmail.com>
2025-05-12 19:40:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("meuBotao").addEventListener("click", function() {
+  document.getElementById("trocap").innerHTML = "Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado.";
+});
